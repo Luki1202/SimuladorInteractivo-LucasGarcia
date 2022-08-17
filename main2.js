cbf2fab4920faf411163460a2d2142cf9ae38540
Lucas Garcia <lucas.garcia1202@icloud.com>
2022-08-17 11:56:02 -0300

    Rediseño de la app y codigo

--- main2.js
+++ main2.js
@@ -1,74 +1,31 @@
-function ingresarMail() {
-  alert("GAMING CENTER");
-  let mail = prompt("Ingrese su mail");
-  while (mail === "" || mail === null) {
-    mail = prompt("Ingrese su nombre");
-  }
-}
-function mostrarProductos() {
-  let producto;
-  do {
-    producto = prompt(" ¿Que productos está buscando? \n1)Procesador INTEL \n2)Procesador AMD \n3)PC GAMER armada");
-  } while (producto != 1 && producto != 2 && producto != 3 && producto != 4 && producto != 5 && producto != 6);
-  switch (producto) {
-    case "1":
-      return "Procesador INTEL";
-    case "2":
-      return "Procesador AMD";
-    case "3":
-      return "PC GAMER armada";
-  }
-}
-function validarPrecio(producto) {
-  if (producto === "Procesador INTEL") {
-    return 10000;
-  } else if (producto === "Procesador AMD") {
-    return 15000;
-  } else {
-    return 150000;
-  }
-}
-function cobrar(producto, precio) {
-  alert(
-    "Usted lleva el siguiente producto :" +
-      producto.toUpperCase() +
-      "\nPrecio $" +
-      precio
-  );
-  }
-
-function medioPago (){
-  let abonar;
-  do {
-    abonar = prompt(" Seleccione medio de pago \n1) EFECTIVO/TRANSFERENCIA \n2) TARJETA DE CREDITO/DEBITO");
-  } while (abonar != 1 && abonar != 2);
-  switch (abonar) {
-    case "1":
-      return "Muchas gracias por su compra, en unos instantes recibira por mail el cupon para completar el pago";
-    case "2":
-      return "Muchas gracias por su compra, en unos instantes recibira por mail las instrucciones para completar el pago";
-  }
-}
-function validarCompra(abonar) {
-  if ( abonar === "EFECTIVO/TRANSFERENCIA") {
-    return "Muchas gracias por su compra, en unos instantes recibira por mail el cupon para completar el pago";
-  } else {
-    return "Muchas gracias por su compra, en unos instantes recibira por mail las instrucciones para completar el pago";
-  }
-}
-
-function instrucciones(abonar) {
-  alert(
-    "IMPORTANTE: \n" + abonar
-  ) 
-}
-
-ingresarMail();
-let productoSeleccionado = mostrarProductos();
-let precioProd = validarPrecio(productoSeleccionado);
-cobrar(productoSeleccionado, precioProd);
-let abonar = medioPago();
-let finalizarCompra = validarCompra(abonar);
-instrucciones(abonar, finalizarCompra);
+const contenedorProductos = document.getElementById('contenedor-productos')
 
 
+let carrito = []
+
+
+
+stockProductos.forEach((producto) => {
+  const div = document.createElement('div')
+  div.classList.add('producto')
+  div.innerHTML = `
+  <img src=${producto.img} alt="">
+  <h3>${producto.nombre}</h3>
+  <p class="precioProducto">Precio:$ ${producto.precio}</p>
+  <button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="fas-fa-shopping-cart"></i></button> 
+  `
+
+  contenedorProductos.appendChild(div)
+
+  const boton = document.getElementById(`agregar${producto.id}`)
+
+  boton.addEventListener('click', () => {
+    agregarAlCarrito(producto.id)
+  })
+})
+
+const agregarAlCarrito = (prodId) => {
+  const item = stockProductos.find((prod) => prod.id === prodId )
+  carrito.push(item)
+  console.log(carrito)
+}
--- stock.js
+++ stock.js
@@ -1,3 +1,36 @@
-const producto = ["Procesador INTEL", "Procesador AMD", "PC GAMER armada"]; //Declaración del array productos
+const stockProductos = [
+    {
+        id: 1,
+        nombre: "Procesador INTEL",
+        tipo: "procesador",
+        cantidad: 1,
+        precio: 8000,
+        img: 'src/img/procesadorIntel.jpg'
+    },
+    {
+        id: 2,
+        nombre: "Procesador AMD",
+        tipo: "procesador",
+        cantidad: 1,
+        precio: 10000,
+        img: 'src/img/procesadorAMD.jpg'
+    },
+    {
+        id: 3,
+        nombre: "Silla Gamer",
+        tipo: "silla",
+        cantidad: 1,
+        precio: 40000,
+        img: 'src/img/sillaGamer.jpg'
+    },
+    {
+        id: 4,
+        nombre: "Notebook Gamer",
+        tipo: "notebook",
+        cantidad: 1,
+        precio: 300000,
+        img: 'src/img/notebookGamer.jpg'
+    }
+]; //Declaración del array productos
 
 
